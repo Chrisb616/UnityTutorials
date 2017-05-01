2efaee311b6b68ed8423aba6685f0addb7b5b56a
Christopher Boynton <cboynton16@gmail.com>
2017-05-01 07:06:22 -0400

    Starting Space Shooter tutorial

--- Roll-a-ball/Assets/Scripts/SpinnerRotate.cs
+++ Roll-a-ball/Assets/Scripts/SpinnerRotate.cs
@@ -4,8 +4,9 @@ using UnityEngine;
 
 public class SpinnerRotate : MonoBehaviour {
 
+	public int speed;
 
 	void Update () {
-		transform.Rotate (new Vector3 (0, 20, 0) * Time.deltaTime);
+		transform.Rotate (new Vector3 (0, 20, 0) * Time.deltaTime * speed);
 	}
 }
