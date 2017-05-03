cdebaa09e5e1ac22fa75edad5d97850a1a8b22ab
Christopher Boynton <cboynton16@gmail.com>
2017-05-03 09:15:21 -0400

    Progress on Space Shooter

--- Space Shooter/Assets/Scripts/PlayerController.cs
+++ Space Shooter/Assets/Scripts/PlayerController.cs
@@ -13,11 +13,26 @@ public class PlayerController : MonoBehaviour {
 	public float tilt;
 	public Boundary boundary;
 
+	public GameObject shot;
+	public Transform shotSpawn;
+
+	public float fireRate;
+	private float nextFire = 0.0f;
+
 	private Rigidbody rb;
 
 	void Start()
 	{
 		rb = GetComponent<Rigidbody>();
+	}
+
+	void Update()
+	{
+		if (Input.GetButton ("Fire1") && Time.time > nextFire)
+		{
+			nextFire = Time.time + fireRate;
+			Instantiate (shot, shotSpawn.position, shotSpawn.rotation, gameObject.transform);
+		}
 	}
 
 	void FixedUpdate()
